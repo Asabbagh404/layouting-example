bfbe211b9556e9029dd0b51bff18f54f96c3483e
Anthony Sabbagh <ash@f4-group.com>
2022-01-12 15:10:56 +0100

    feat: add layout system

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,25 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <component :is="currentPage"></component>
   </div>
 </template>
+<script>
+import defaultLayout from '@/views/layout/defaultLayout.vue'
+import noLayout from '@/views/layout/noLayout.vue'
 
+export default {
+  component: {
+    defaultLayout,
+    noLayout
+  },
+  computed: {
+    currentPage() {
+      return !this.$route.meta.layout ? defaultLayout : noLayout
+    }
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
